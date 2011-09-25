a763b12d7148707a63548074b7310e82f8815c0f
leland.unnerd@gmail.com <leland.unnerd@gmail.com@d5f0d992-e2fe-9f75-c005-68fe73da8c1c>
2011-09-25 08:24:05 +0000

    Ito na

---
+++ trunk/Omnipresence/Omnipresence.Mvc2/Models/Account/AccountModels.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Omnipresence.Mvc2.Models
+{
+    public class LoginViewModel
+    {
+        // TODO: Confirm actual LOGIN information, and update this (LoginViewModel)
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+    public class NewAccountViewModel
+    {
+        // TODO: Confirm actual REGISTRATION information, and update this (NewAccountViewModel)
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+    }
+    public class ShortAccountViewModel
+    {
+        public string PictureThumbnail { get; set; }
+        public string DisplayName { get; set; }
+        public string UserName { get; set; }
+        public string Title { get; set; }
+        public int UserId { get; set; }
+    }
+    public class ProfileViewModel
+    {
+        // TODO: Update to include actual profile information
+        public string Username { get; set; }
+    }
+}
