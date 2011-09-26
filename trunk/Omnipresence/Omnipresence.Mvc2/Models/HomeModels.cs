a58d035a9f60e739024ba90b3d738bdb0e0003db
leland.unnerd@gmail.com <leland.unnerd@gmail.com@d5f0d992-e2fe-9f75-c005-68fe73da8c1c>
2011-09-26 01:28:48 +0000

    Resolved conflict!

---
+++ trunk/Omnipresence/Omnipresence.Mvc2/Models/HomeModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Omnipresence.Mvc2.Models
+{
+    public class IndexViewModel
+    {
+        //TODO: Update this :S
+    }
+}
