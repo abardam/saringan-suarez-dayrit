e61fa501df253ca2a6dbba9bb7ffb2f4ea555ed9
leland.unnerd@gmail.com <leland.unnerd@gmail.com@d5f0d992-e2fe-9f75-c005-68fe73da8c1c>
2011-12-10 00:03:33 +0000

    changed profilecontroller to add profile page

--- trunk/Omnipresence/Omnipresence.Mvc2/Controllers/ProfileController.cs
+++ trunk/Omnipresence/Omnipresence.Mvc2/Controllers/ProfileController.cs
@@ -4,22 +4,41 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Omnipresence.Mvc2.Models;
+using Omnipresence.Processing;
 
 namespace Omnipresence.Mvc2.Controllers
 {
     public class ProfileController : Controller
     {
+        private AccountServices accountServices;
+
+        protected override void Initialize(System.Web.Routing.RequestContext requestContext)
+        {
+            base.Initialize(requestContext);
+            accountServices = AccountServices.GetInstance();
+        }
         public ProfileViewModel GetProfile(int id)
         {
             // TODO: This is still empty model
             ProfileViewModel model = new ProfileViewModel { AvatarUrl = "viewprofile.png", Birthdate = DateTime.Now, Description = "Super bad-ass", FirstName = "Adrian", GenderText = "Male", LastName = "Fazinsky", Reputation = 10 };
             return model;
         }
-        public ActionResult Index()
-        {
-            ViewData["Name"] = User.Identity.Name;
 
-            return View();
+        public ActionResult Index(int id = 0)
+        {
+            //TODO: Actual model
+            if (id == 0)
+            {
+                UserProfileModel profile = accountServices.GetUserProfileByUsername(User.Identity.Name);
+                if (profile == null) return RedirectToAction("Index", "Home");
+                return View(profile);
+            }
+            else
+            {
+                UserProfileModel profile = accountServices.GetUserProfileById(id);
+                if (profile != null) return View(profile);
+                else return RedirectToAction("Index", "Home");
+            }
         }
 
         //
