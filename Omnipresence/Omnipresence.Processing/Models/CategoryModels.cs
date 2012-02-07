d42539600066d37d598f37f4d70e7441b005ebb7
saringan.emanuel@gmail.com <saringan.emanuel@gmail.com@d5f0d992-e2fe-9f75-c005-68fe73da8c1c>
2012-02-07 15:30:33 +0000

    Added GetAllCategories() method to CategoryServices

--- Omnipresence/Omnipresence.Processing/Models/CategoryModels.cs
+++ Omnipresence/Omnipresence.Processing/Models/CategoryModels.cs
@@ -5,6 +5,14 @@ using System.Text;
 
 namespace Omnipresence.Processing
 {
+    public class CategoryModel
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string IconPath { get; set; }
+    }
+
     public class CreateCategoryModel
     {
         public int CategoryId { get; set; }
