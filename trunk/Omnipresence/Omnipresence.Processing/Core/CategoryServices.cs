9d3f900537aa320fcb1e43a2ab2be1ba8fbbbe29
saringan.emanuel@gmail.com <saringan.emanuel@gmail.com@d5f0d992-e2fe-9f75-c005-68fe73da8c1c>
2012-02-07 15:13:16 +0000

    Added CategoryServices

---
+++ trunk/Omnipresence/Omnipresence.Processing/Core/CategoryServices.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Omnipresence.DataAccess.Core;
+
+namespace Omnipresence.Processing
+{
+    public class CategoryServices
+    {
+        #region [FIELDS]
+
+        private OmnipresenceEntities db;
+        private static CategoryServices instance;
+
+        #endregion
+
+        #region [CONSTRUCTOR]
+
+        private CategoryServices()
+        {
+            db = new OmnipresenceEntities();
+            db.Connection.Open();
+        }
+
+        public static CategoryServices GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new CategoryServices();
+            }
+
+            return instance;
+        }
+
+        #endregion
+
+        public bool CreateCategory(CreateCategoryModel ccm)
+        {
+            Category c = new Category();
+            c.Name = ccm.Name;
+            c.Description = ccm.Description;
+            c.IconPath = ccm.IconPath;
+
+            db.Categories.AddObject(c);
+            db.SaveChanges();
+
+            return true;
+        }
+    }
+}
