1c285b438a4d65b4b03023bee754488f3efb44d9
saringan.emanuel@gmail.com <saringan.emanuel@gmail.com@d5f0d992-e2fe-9f75-c005-68fe73da8c1c>
2012-02-07 15:13:16 +0000

    Added CategoryServices

---
+++ Omnipresence/Omnipresence.Processing/Models/CategoryModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Omnipresence.Processing
+{
+    public class CreateCategoryModel
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string IconPath { get; set; }
+    }
+}
