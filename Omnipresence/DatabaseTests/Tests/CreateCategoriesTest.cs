6dd24cac8517b5cfca0fea1458f13562d4805473
saringan.emanuel@gmail.com <saringan.emanuel@gmail.com@d5f0d992-e2fe-9f75-c005-68fe73da8c1c>
2012-02-09 19:41:57 +0000

    Edited Category Creation to prevent duplicate Categories.

--- Omnipresence/DatabaseTests/Tests/CreateCategoriesTest.cs
+++ Omnipresence/DatabaseTests/Tests/CreateCategoriesTest.cs
@@ -15,21 +15,24 @@ namespace DatabaseTests
 
         public override bool Execute()
         {
-            CreateCategoryModel ccm = new CreateCategoryModel();
-            ccm.Name = "Party";
-            ccm.Description = "This is A Party Event Description";
-            ccm.IconPath = "/Content/Images/party.png";
-            categoryServices.CreateCategory(ccm);
+            if (categoryServices.GetAllCategories().Count() == 0)
+            {
+                CreateCategoryModel ccm = new CreateCategoryModel();
+                ccm.Name = "Party";
+                ccm.Description = "This is A Party Event Description";
+                ccm.IconPath = "/Content/Images/party.png";
+                categoryServices.CreateCategory(ccm);
 
-            ccm.Name = "Disaster";
-            ccm.Description = "This is A Disaster Event Description";
-            ccm.IconPath = "/Content/Images/disaster.png";
-            categoryServices.CreateCategory(ccm);
+                ccm.Name = "Disaster";
+                ccm.Description = "This is A Disaster Event Description";
+                ccm.IconPath = "/Content/Images/disaster.png";
+                categoryServices.CreateCategory(ccm);
 
-            ccm.Name = "Traffic";
-            ccm.Description = "This is A Traffic Event Description";
-            ccm.IconPath = "/Content/Images/traffic.png";
-            categoryServices.CreateCategory(ccm);
+                ccm.Name = "Traffic";
+                ccm.Description = "This is A Traffic Event Description";
+                ccm.IconPath = "/Content/Images/traffic.png";
+                categoryServices.CreateCategory(ccm);
+            }
 
             return true;
         }
